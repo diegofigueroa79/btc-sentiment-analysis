893fc918a4fef77204ba54ec0fff04773a615afc
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-12-30 11:37:33 -0800

    fixed missing self param for article methods

--- btc-sentiment-analysis/btc_sentiment_analysis/article.py
+++ btc-sentiment-analysis/btc_sentiment_analysis/article.py
@@ -1,4 +1,4 @@
-from .analyzers import vader_analyze, blob_analyze
+from analyzers import vader_analyze, blob_analyze
 
 class Article:
 	
@@ -9,12 +9,12 @@ class Article:
 		self.link = link
 		self.text = text
 	
-	def vader_analysis():
+	def vader_analysis(self):
 		title_sentiment = vader_analyze(self.title)
 		text_sentiment = vader_analyze(self.text)
 		return {'title': title_sentiment, 'text': text_sentiment}
 	
-	def blob_analysis():
+	def blob_analysis(self):
 		title_sentiment = blob_analyze(self.title)
 		text_sentiment = blob_analyze(self.text)
 		return {'title': title_sentiment, 'text': text_sentiment}
